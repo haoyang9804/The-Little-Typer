a06f141f5197e73c394c97b7f2d971cbf814a474
haoyang9804 <haoyanghkust@gmail.com>
2022-05-30 18:50:26 +0800

    May 30 2022

--- proof.rkt
+++ proof.rkt
@@ -27,3 +27,166 @@
         (same (add1 n))))
 
 (+1=add1 13)
+
+(claim incr
+    (-> Nat Nat))
+
+(define incr
+    (lambda (n)
+        (iter-Nat n
+            1
+            (+ 1))))
+
+(claim incr=add1
+    (Pi ((n Nat))
+        (= Nat (incr n) (add1 n))))
+
+(claim base-incr=add1
+    (= Nat (incr 0) (add1 0)))
+
+(define base-incr=add1
+    (same (add1 0)))
+
+(claim mot-incr=add1
+    (-> Nat U))
+
+(define mot-incr=add1
+    (lambda (n)
+        (= Nat (incr n) (add1 n))))
+
+; (claim step-incr=add1
+;     (Pi ((n Nat))
+;         (-> (mot-incr=add1 n)
+;             (mot-incr=add1 (add1 n)))))
+
+; (define step-incr=add1
+;     (lambda (l+1)
+;         (lambda (almost-func)
+;             (almost-func ))))
+
+(claim step-incr=add1
+    (Pi ((n-1 Nat))
+        (-> (= Nat
+                (incr n-1)
+                (add1 n-1))
+            (= Nat
+                (add1
+                    (incr n-1))
+                (add1
+                    (add1 n-1))))))
+
+(define step-incr=add1
+    (lambda (n-1)
+        (lambda (incr=add1_n-1)
+            (cong incr=add1_n-1 (+ 1)))))
+
+(define incr=add1
+    (lambda (n)
+        (ind-Nat n
+            mot-incr=add1
+            base-incr=add1
+            step-incr=add1)))
+
+(incr=add1 0)
+
+(claim step-incr=add1-2
+    (Pi ((n-1 Nat))
+        (-> (= Nat
+                (incr n-1)
+                (add1 n-1))
+            (= Nat
+                (add1
+                    (incr n-1))
+                (add1
+                    (add1 n-1))))))
+
+; (claim target-incr=add1-2
+;     (Pi ((n Nat))
+;         ()))
+
+(claim mot-step-incr=add1-2
+    (-> Nat Nat
+        U))
+
+(define mot-step-incr=add1-2
+    (lambda (n-1 k)
+        (= Nat
+            (add1
+                (incr n-1))
+            (add1
+                k))))
+
+(define step-incr=add1-2
+    (lambda (n-1)
+        (lambda (incr=add1-2_n-1)
+            (replace incr=add1-2_n-1
+                (mot-step-incr=add1-2 n-1)
+                (same (add1 (incr n-1)))))))
+
+(claim incr=add1-2
+    (Pi ((n Nat))
+        (= Nat (incr n) (add1 n))))
+
+(define incr=add1-2
+    (lambda (n)
+        (ind-Nat n
+            mot-incr=add1
+            base-incr=add1
+            step-incr=add1-2)))
+
+(incr=add1-2 20)
+
+(claim double
+    (-> Nat Nat))
+
+(define double
+    (lambda (n)
+        (iter-Nat n
+            0
+            (+ 2))))
+
+(claim twice
+    (-> Nat Nat))
+
+(define twice
+    (lambda (n))
+        (+ n n))
+
+(claim double=twice
+    (Pi ((n Nat))
+        (= Nat (double n) (twice n))))
+
+(claim mot-double=twice
+    -> Nat U)
+
+(define mot-double=twice
+    (lambda (n)
+        (= Nat (double n) (twice n))))
+
+(claim base-double=twice
+    (= Nat (double 0) (twice 0)))
+
+(define base-double=twice
+    (same (add1 0)))
+
+(claim step-double=twice
+    (Pi ((n-1 Nat))
+        (-> 
+            (= Nat (double n-1) (twice n-1))
+            (= Nat (add1 (add1 (double n-1))) (add1 (add1 (twice n-1)))))))
+
+(define step-double=twice
+    (lambda (n-1)
+        (lambda (almost_answer_n-1)
+            (replace
+                almost_answer_n-1
+                (lambda (k)
+                    (= Nat (add1 (add1 (double n-1))) (add1 (add1 k)))
+                (same (add1 (add1 (double n-1)))))))))
+
+(define double=twice
+    (lambda (n)
+        (ind-Nat n
+            mot-double=twice
+            base-double=twice)))
+
